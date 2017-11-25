f15ca77924515eecce6d7884299d2deb8bffc4c1
Juha Ojanperä <juha.ojanpera@gmail.com>
2017-11-25 19:19:02 +0200

    Switch to use vue-test-utils as the unit testing utility library.

--- src/components/__tests__/HelloWorld.spec.js
+++ src/components/__tests__/HelloWorld.spec.js
@@ -1,11 +1,10 @@
-import Vue from 'vue';
+import { shallow } from 'vue-test-utils';
 import HelloWorld from '@/components/HelloWorld';
 
+
 describe('HelloWorld.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld);
-    const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector('.hello h1').textContent)
-    .toEqual('Welcome to Your Vue.js App');
-  });
+    it('should render correct contents', () => {
+        const wrapper = shallow(HelloWorld);
+        expect(wrapper.find('.hello h1').text()).toEqual('Welcome to Your Vue.js App');
+    });
 });
