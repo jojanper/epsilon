a5979285aea71d438bbe15b0b87cc4deb8cac9ae
Juha Ojanperä <juha.ojanpera@gmail.com>
2018-08-06 14:25:48 +0300

    Header and E2E tests update.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,46 @@
 <template>
   <div id="app">
-    <header class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <router-link class="navbar-brand" :to="{ name: 'home' }">Epsilon</router-link>
-      <ul class="nav navbar-nav">
-        <li class="nav-item">
-          <router-link class="nav-link" active-class="active" exact :to="{ name: 'home' }">
-            Home
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" active-class="active" exact :to="{ name: 'about' }">
-            About
-          </router-link>
-        </li>
-      </ul>
-    </header>
+      <button class="navbar-toggler" type="button" data-toggle="collapse"
+        data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li v-for="(route, index) in routes" :key="index" class="nav-item">
+            <router-link class="nav-link" data-toggle="collapse" data-target=".navbar-collapse.show"
+              active-class="active" exact :to="{ name: route.name }">
+              {{ route.title}}
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+        return {
+            routes: [
+                {
+                    name: 'home',
+                    title: 'Home'
+                },
+                {
+                    name: 'about',
+                    title: 'About'
+                }
+            ]
+        };
+    }
+};
+</script>
 
 <style lang="scss">
 #app {
--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,37 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
-    it('Visits the app root url', () => {
-        cy.visit('/');
-        cy.contains('h1', 'Welcome to Your Vue.js App');
+const homeTitle = 'Welcome to Your Vue.js App';
+const aboutTitle = 'This is an about page';
+
+
+describe('Main view', () => {
+    it('contains menu links', () => {
+        cy.visit('#/');
+        cy.get('a.nav-link').should('have.length', 2);
+    });
+
+    it('first link is clicked', () => {
+        cy.visit('#/about');
+        cy.get('a.nav-link').eq(0).click();
+        cy.contains('h1', homeTitle);
+    });
+
+    it('second link is clicked', () => {
+        cy.get('a.nav-link').eq(1).click();
+        cy.contains('h1', aboutTitle);
+    });
+});
+
+describe('Home page', () => {
+    it('exists', () => {
+        cy.visit('#/');
+        cy.contains('h1', homeTitle);
+    });
+});
+
+describe('About page', () => {
+    it('exists', () => {
+        cy.visit('#/about');
+        cy.contains('h1', aboutTitle);
     });
 });
