039e61448dbe7705400de63e2981a9c43a671141
Juha Ojanperä <juha.ojanpera@gmail.com>
2018-08-09 15:35:20 +0300

    Footer HTML.

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,13 @@
     <div class="container">
       <router-view/>
     </div>
+    <footer class="footer">
+      <div class="container">
+        <span class="text-muted">
+            <a href="https://github.com/jojanper/epsilon" target="_blank">Epsilon powered by Vue</a>
+        </span>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -38,5 +45,13 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   text-align: center;
   color: #2c3e50;
+}
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 60px; /* Set the fixed height of the footer here */
+  line-height: 60px; /* Vertically center the text there */
+  background-color: #f5f5f5;
 }
 </style>
