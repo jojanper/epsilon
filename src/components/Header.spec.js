343df239f2b152a3587e053d1f5fdc19797b8ff3
Juha Ojanperä <juha.ojanpera@gmail.com>
2018-11-01 15:03:31 +0200

    Clean up.

--- src/components/Header.spec.js
+++ src/components/Header.spec.js
@@ -6,20 +6,6 @@ import { mount, shallowMount, RouterLinkStub, createLocalVue } from '@vue/test-u
 
 import DraalHeader from './Header.vue';
 
-const { getComputedStyle } = window
-window.getComputedStyle = function getComputedStyleStub(el) {
-	return {
-        ...getComputedStyle(el),
-		transitionDelay: '',
-		transitionDuration: '',
-		animationDelay: '',
-        animationDuration: '',
-        getPropertyValue() {
-            return '';
-        }
-	}
-}
-
 Vue.use(Vuetify);
 Vue.use(VueI18n);
 Vue.use(Vuex);
@@ -32,22 +18,6 @@ const i18n = new VueI18n({
         fi: {}
     }
 });
-
-/**
- * Adds a warapping `div data-app="true"` to the body so that we don't
- * get Vuetify complaining about missing data-app attribute for some components.
- *
- * @return undefined
- */
-/*
-function addElemWithDataAppToBody() {
-    const app = document.createElement('div');
-    app.setAttribute('data-app', true);
-    document.body.append(app);
-};
-
-addElemWithDataAppToBody();
-*/
 
 describe('DraalHeader', () => {
     let localVue;
@@ -67,7 +37,6 @@ describe('DraalHeader', () => {
     });
 
     it('renders correctly', () => {
-        //localVue = createLocalVue();
         const wrapper = shallowMount(DraalHeader, {
             localVue,
             propsData: props,
@@ -77,13 +46,11 @@ describe('DraalHeader', () => {
         });
         const elements = wrapper.findAll('a');
         expect(elements.length).toEqual(2);
-        //console.log(wrapper.html());
         expect(elements.at(0).text()).toMatch(props.appName);
         expect(elements.at(1).text()).toMatch(props.routes[0].title);
     });
 
     it('user can change language', () => {
-        //localVue = createLocalVue();
         const App = localVue.component('TestDraalHeader', {
             components: {
                 DraalHeader
@@ -96,32 +63,26 @@ describe('DraalHeader', () => {
                 <draal-header :routes="routes" :appName="appName"></draal-header>
               </v-app>
             `
-          });
+        });
 
-        const state = {
-            foo: []
-        };
-        const getters = {};
-
-        const actions = {
-            setLang(state, obj) {
-                console.log('Set lang HIPPII', obj.lang);
-                //console.log(obj);
-            }
-        };
+        let selectedLang = null;
 
         const store = new Vuex.Store({
             modules: {
                 app: {
                     namespaced: true,
-                    state,
-                    actions,
-                    getters
+                    state: {},
+                    actions: {
+                        setLang(_state, obj) {
+                            selectedLang = obj.lang;
+                        }
+                    },
+                    getters: {}
                 }
             }
         });
 
-        const wrapper = mount(App/*DraalHeader*/, {
+        const wrapper = mount(App, {
             store,
             localVue,
             propsData: props,
@@ -129,62 +90,19 @@ describe('DraalHeader', () => {
                 RouterLink: RouterLinkStub,
                 DraalHeader
             },
-            /*
-            mocks: {
-                $store: {
-                    modules: {
-                        app: {
-                            state: {},
-                            getters: {},
-                            actions: {
-                                setLang() {
-                                    console.log('Set lang');
-                                }
-                            }
-                        }
-                    }
-                }
-            },
-            */
             i18n,
-            attachToDocument: true,
-            //sync: false
+            attachToDocument: true
         });
+
+        // User open the language selection menu
         const elements = wrapper.findAll('button');
-        //console.log(elements.length);
-        //console.log(elements.at(1).html());
-        console.log(wrapper.html());
-
         elements.at(1).trigger('click');
-        //setTimeout(() => {
 
-            //wrapper.vm.$nextTick(() => {
+        // And clicks the second language
+        const langEls = wrapper.findAll('a.v-list__tile');
+        langEls.at(1).trigger('click');
 
-            //console.log(document.body.innerHTML);
-                //console.log(wrapper.html());
-                //console.log(wrapper.vm.$store);
-
-                const langEls = wrapper.findAll('a.v-list__tile');
-                //console.log(langEls.at(0).html());
-                //console.log(langEls.at(1).html());
-
-                //console.log(langEls.at(0));
-
-                //console.log(wrapper.vm);
-                langEls.at(1).trigger('click');
-
-            //console.log(document.body.innerHTML);
-            //expect(elements.length).toEqual(2);
-            //expect(elements.at(0).text()).toMatch(props.appName);
-            //expect(elements.at(1).text()).toMatch(props.routes[0].title);
-
-            //const el = document.getElementsByClassName('v-list__tile')
-            //console.log(el.length);
-
-            //el[0].dispatchEvent(new Event('click'));
-
-                //done();
-            //});
-        //}, 1000);
+        // Language is selected
+        expect(selectedLang).toEqual('fi');
     });
 });
