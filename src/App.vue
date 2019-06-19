dedf4bbd50aae17c73549bdec2fa8094512e9734
Juha Ojanperä <juha.ojanpera@gmail.com>
2019-06-19 15:18:58 +0300

    Upgrade dependencies (#16)

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,26 @@
     <draal-header :routes="header.routes" :appName="header.appName"></draal-header>
     <draal-notification></draal-notification>
     <div class="container">
-
-      <i class="icon ion-md-create text-success ml-2"></i><span>&nbsp;Sign up</span>
-      <i class="icon ion-md-log-in text-primary ml-2"></i><span>&nbsp;Login</span>
-      <i class="icon ion-md-log-out text-primary ml-2"></i><span>&nbsp;Logout</span>
+      <i class="icon ion-md-create text-success ml-2"></i>
+      <span>&nbsp;Sign up</span>
+      <i class="icon ion-md-log-in text-primary ml-2"></i>
+      <span>&nbsp;Login</span>
+      <i class="icon ion-md-log-out text-primary ml-2"></i>
+      <span>&nbsp;Logout</span>
 
       <router-view/>
       <v-flex xs12 sm34 text-xs-center>
-      <v-btn color="primary" v-on:click="addAlert('Success')">Add success alert</v-btn>
-      <v-btn color="info" v-on:click="addAlert('Info')">Add info alert</v-btn>
-      <v-btn color="warning" v-on:click="addAlert('Warning')">Add warning alert</v-btn>
-      <v-btn color="error" v-on:click="addAlert('Error')">Add error alert</v-btn>
+        <v-btn color="primary" v-on:click="addAlert('Success')">Add success alert</v-btn>
+        <v-btn color="info" v-on:click="addAlert('Info')">Add info alert</v-btn>
+        <v-btn color="warning" v-on:click="addAlert('Warning')">Add warning alert</v-btn>
+        <v-btn color="error" v-on:click="addAlert('Error')">Add error alert</v-btn>
       </v-flex>
       <ul class="list-group">
-        <li v-for="(item, index) in data" :key="index" class="list-group-item">
-            {{ item.date }} - {{ item.close }}
-        </li>
+        <li
+          v-for="(item, index) in data"
+          :key="index"
+          class="list-group-item"
+        >{{ item.headline }} - {{ item.summary }}</li>
       </ul>
     </div>
     <draal-footer :link="footer.link" :title="footer.name"></draal-footer>
@@ -48,7 +52,7 @@ export default {
 
     created() {
         IEXApi.stock('aapl').subscribe(
-            data => data.chart.forEach(chart => this.data.push(chart)),
+            data => data.news.forEach(news => this.data.push(news)),
             dummyErrorHandler
         );
     },
@@ -81,7 +85,9 @@ export default {
         addAlert(mode) {
             const timeout = Math.floor(Math.random() * 5000);
             const msg = `${mode} notification. Timeout ${timeout}msec`;
-            this.addNotification(NotificationMessage[`create${mode}`](msg, { timeout }));
+            this.addNotification(
+                NotificationMessage[`create${mode}`](msg, { timeout })
+            );
         }
     }
 };
@@ -90,14 +96,14 @@ export default {
 <style lang="scss" scoped>
 // Style loading bar between pages.
 // https://github.com/rstacruz/nprogress
-@import '~nprogress/nprogress.css';
+@import "~nprogress/nprogress.css";
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   text-align: center;
   color: #2c3e50;
 }
 .container {
-    min-height: 400px;
+  min-height: 400px;
 }
 </style>
