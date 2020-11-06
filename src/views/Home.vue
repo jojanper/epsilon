1302ecb1b8a0e0a289b19c087dff73ebd4ab95f5
Juha Ojanperä <juha.ojanpera@gmail.com>
2020-11-06 21:27:56 +0200

    Fix E2E tests.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png" />
+  <div>
+    <h1>Epsilon</h1>
+    <div class="home">
+      <img src="../assets/logo.png" />
+    </div>
   </div>
 </template>
 
