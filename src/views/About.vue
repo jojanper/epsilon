1b4e770201055de10305db8ce45a397241d5b00a
Juha Ojanperä <juha.ojanpera@gmail.com>
2020-12-29 18:55:51 +0200

    Read audio file data.

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,19 +3,27 @@
     <h1>This is an about page</h1>
     <draal-nested-view class="w-25" :nodes="list"></draal-nested-view>
 
-    <canvas width="1000" height="200" ref="canvas"></canvas>
+    <canvas width="1000" height="400" ref="canvas"></canvas>
+
+    <draal-file-dialog color="blue" icon="mdi-folder-open" @file-select="fileSelect"></draal-file-dialog>
   </div>
 </template>
 
 <script>
 import DraalNestedView from './Nested.vue';
+import DraalFileDialog from '@/components/core/utils/FileDialog.vue';
 
 import { peaks } from './peaks';
+import { getMediaDuration } from '@/common/utils';
 
 export default {
-    components: { DraalNestedView },
+    components: {
+        DraalNestedView,
+        DraalFileDialog
+    },
     data() {
         return {
+            fileDialog: false,
             list: [
                 {
                     text: 'Foo',
@@ -63,11 +71,11 @@ export default {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
         const first = 0;
-        const absmaxHalf = 0.015625;
+        const absmaxHalf = 0.125 * 0.015625; // 0.015625;
         const canvasStart = 0;
         const canvasEnd = 1742;
-        const scale = 0.5;
-        const halfOffset = 64;
+        const scale = 1.0;
+        const halfOffset = 200;
 
         this.halfPixel = 0.5;
 
@@ -96,6 +104,22 @@ export default {
         //this.ctx.restore();
         this.ctx.closePath();
         this.ctx.fill();
+    },
+    methods: {
+        fileSelect(files) {
+            console.log('selected', files);
+
+            getMediaDuration(
+                files[0],
+                data => {
+                    console.log(data);
+                    console.log(data.getChannelData(0));
+                },
+                err => {
+                    console.log(err);
+                }
+            );
+        }
     }
 };
 </script>
