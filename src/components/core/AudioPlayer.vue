30f5a5ea4e5dde8b321611b7ab5b9587e3a37e2a
Juha Ojanperä <juha.ojanpera@gmail.com>
2021-01-03 15:36:29 +0200

    Audio player component supports event markers.

--- src/components/core/AudioPlayer.vue
+++ src/components/core/AudioPlayer.vue
@@ -57,10 +57,41 @@
       :disabled="!loaded"
     ></v-progress-linear>
 
-    <!--
-      @slot Default slot.
-    -->
-    <slot name="default"></slot>
+    <div class="position-relative mt-3 mb-3 pb-3">
+      <div v-for="(item, i) in events" :key="i">
+        <v-hover v-slot="{ hover }" open-delay="400">
+          <div class="audio-event-wrapper" :style="getEventPosStyle(item)">
+            <!--
+                @slot Event marker slot.
+            -->
+            <slot name="marker">
+              <v-icon :color="color" class="audio-event-marker">mdi-map-marker</v-icon>
+            </slot>
+            <div v-if="hover" class="position-absolute" style="top: 20px;">
+              <!--
+                @slot Event slot.
+                @binding {number} eventIndex Event index.
+                @binding {object} event Event data.
+              -->
+              <slot name="event" v-bind:eventIndex="i" v-bind:event="item">
+                <v-card class="p-1 z-index-10" color="grey lighten-4">
+                  <v-card-text class="p-2">
+                    <div class="p-0 font-weight-light title">
+                      <!--
+                      @slot Event item render slot.
+                      @binding {number} eventIndex Event index.
+                      @binding {object} event Event data.
+                      -->
+                      <slot name="item" v-bind:eventIndex="i" v-bind:event="item">{{ item }}</slot>
+                    </div>
+                  </v-card-text>
+                </v-card>
+              </slot>
+            </div>
+          </div>
+        </v-hover>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -102,6 +133,15 @@ export default {
             type: String,
             required: false,
             default: 'light-blue lighten-3'
+        },
+        /**
+         * Audio event positions. Event markers are specified below audio timeline
+         * and the parent can customize the event rendering to specific needs.
+         */
+        events: {
+            type: Array,
+            required: false,
+            defalut: () => []
         }
     },
     data() {
@@ -266,6 +306,14 @@ export default {
             const ss = (seconds < 10) ? `0${seconds}` : `${seconds}`;
 
             return `${hh}:${mm}:${ss}`;
+        },
+
+        /**
+         * Set event marker position.
+         */
+        getEventPosStyle(pos) {
+            const duration = (pos / this.duration) * 100;
+            return `--left: ${duration}%`;
         }
     }
 };
@@ -279,6 +327,24 @@ export default {
 
 .v-application a {
   color: inherit !important;
+}
+
+.audio-event-wrapper {
+    top: -8px;
+    position: absolute;
+    width: 100%;
+    left: var(--left);
+}
+
+.audio-event-marker {
+    position: absolute;
+    font-size: 24px;
+    left: -12px;
+    transform: rotate(180deg);
+}
+
+.z-index-10 {
+    z-index: 10
 }
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,55 +5,9 @@
       <img src="../assets/logo.png" />
     </div>
 
-    <!--v-icon class="timeline-entry" :style="getEventPosStyle(item)">mdi-map-marker-outline</v-icon-->
-
-    <draal-audio-player class="mb-3" :name="url" :url="url" @audio-duration="setDuration">
-      <div class="position-relative mt-3 mb-3 pb-3">
-        <div v-for="(item, i) in eventPos" :key="i">
-          <v-hover v-slot="{ hover }" open-delay="400">
-            <div class="audio-event-wrapper" :style="getEventPosStyle(item)">
-              <v-icon color="blue darken-1" class="audio-event-marker">mdi-map-marker</v-icon>
-              <div v-if="hover" class="position-absolute" style="top: 20px;">
-                <v-card class="p-1 z-index-10" color="grey lighten-4">
-                  <v-card-text class="p-2">
-                    <div class="p-0 font-weight-light title">{{ item }}</div>
-                  </v-card-text>
-                </v-card>
-              </div>
-            </div>
-          </v-hover>
-        </div>
-      </div>
+    <draal-audio-player class="mb-3" :name="url" :url="url" :events="eventPos">
+      <template v-slot:item="{ event }">{{ event }}s</template>
     </draal-audio-player>
-
-    <!--draal-audio-player class="mb-3" :name="url" :url="url">
-      <div class="position-relative mt-3 mb-3 pb-3">
-        <div v-for="(item, i) in eventPos" :key="i">
-          <v-hover v-slot="{ hover }" open-delay="400">
-            <div class="timeline-entry" :style="getEventPosStyle(item)">
-              <div
-                v-if="hover"
-                class2="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
-                class="d2-flex darken2-2"
-                style="position: absolute; height: 100%; top: 20px;"
-              >
-                <v-card
-                  class="p-1"
-                  class2="mx-auto"
-                  color="grey lighten-4"
-                  max-width2="600"
-                  style="z-index: 100; "
-                >
-                  <v-card-text class="p-2" style2="position: relative;">
-                    <div class="p-0 font-weight-light title mb-2">{{ item }}</div>
-                  </v-card-text>
-                </v-card>
-              </div>
-            </div>
-          </v-hover>
-        </div>
-      </div>
-    </draal-audio-player-->
   </div>
 </template>
 
@@ -75,65 +29,8 @@ export default {
                 2,
                 10,
                 70
-            ],
-            duration: 0
+            ]
         };
-    },
-    methods: {
-        setDuration(duration) {
-            this.duration = duration;
-        },
-
-        getEventPosStyle(pos) {
-            const duration = (pos / this.duration) * 100;
-            return `--left: ${duration}%`;
-        }
     }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-/*
-.timeline-entry {
-    top: 0px;
-    position: absolute;
-    border-left: 4px solid red;
-    height: 15px;
-    width: 10%;
-    left: var(--left);
-    transform2: rotate(180deg);
-}
-*/
-
-.audio-event-wrapper {
-    top: -8px;
-    position: absolute;
-    // border-left: 1px solid red;
-    //height: 15px;
-    width: 100%;
-    left: var(--left);
-    //transform2: rotate(180deg);
-}
-.audio-event-marker {
-    position: absolute;
-    font-size: 24px;
-    left: -12px;
-    // color: red;
-    //font-size: 20px;
-
-  /*
-    top: 0px;
-    position: absolute;
-    border-left: 4px solid red;
-    height: 15px;
-    width: 10%;
-    left: var(--left);
-    */
-    transform: rotate(180deg);
-}
-
-.z-index-10 {
-    z-index: 10
-}
-</style>
