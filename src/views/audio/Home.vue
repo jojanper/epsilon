91f110b34d310682627dbaaeca3f0e10cf41b44a
Juha Ojanperä <juha.ojanpera@gmail.com>
2021-02-05 13:33:32 +0200

    Remove example audio peaks file.

--- src/views/audio/Home.vue
+++ src/views/audio/Home.vue
@@ -45,8 +45,6 @@
 </template>
 
 <script>
-// import { peaks } from './peaks';
-
 import DraalFileImport from '@/components/core/utils/FileImport.vue';
 import DraalIconDialog from '@/components/core/utils/IconDialog.vue';
 import DraalExpandItem from '@/components/core/utils/ExpandItem.vue';
@@ -81,47 +79,6 @@ export default {
     },
     mounted() {
         this.canvas = this.$refs.canvas;
-        /*
-        this.canvas = this.$refs.canvas;
-        this.ctx = this.canvas.getContext('2d');
-
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-        const first = 0;
-        const absmaxHalf = 0.125 * 0.015625; // 0.015625;
-        const canvasStart = 0;
-        const canvasEnd = 1742;
-        const scale = 1.0;
-        const halfOffset = 200;
-
-        this.halfPixel = 0.5;
-
-        this.ctx.beginPath();
-        this.ctx.fillStyle = 'violet';
-        // this.ctx.moveTo((canvasStart - first) * scale, halfOffset);
-
-        let i;
-        for (i = canvasStart; i < canvasEnd; i++) {
-            const peak = peaks[2 * i] || 0;
-            const h = Math.round(peak / absmaxHalf);
-            console.log((i - first) * scale + this.halfPixel, halfOffset - h);
-            this.ctx.lineTo((i - first) * scale /* + this.halfPixel *, halfOffset - h);
-        }
-
-        // draw the bottom edge going backwards, to make a single
-        // closed hull to fill
-        let j = canvasEnd - 1;
-        for (j; j >= canvasStart; j--) {
-            const peak = peaks[2 * j + 1] || 0;
-            const h = Math.round(peak / absmaxHalf);
-            // console.log((j - first) * scale + this.halfPixel, halfOffset - h);
-            this.ctx.lineTo((j - first) * scale + 1 * this.halfPixel, halfOffset - h);
-        }
-
-        // this.ctx.restore();
-        this.ctx.closePath();
-        this.ctx.fill();
-        */
     },
     methods: {
         fileSelect(files) {
@@ -230,8 +187,6 @@ export default {
                 for (let ch = 0; ch < numberOfChannels; ch++) {
                     minPeaks[ch].push(minVal[ch]);
                     maxPeaks[ch].push(maxVal[ch]);
-                    minVal[ch] = Infinity;
-                    maxVal[ch] = -Infinity;
                 }
             }
 
