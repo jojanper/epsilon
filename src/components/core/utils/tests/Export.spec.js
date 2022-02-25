7d450bc6807548bdcf0b318ecebae2e6bd236936
Juha Ojanperä <juha.ojanpera@gmail.com>
2022-02-25 19:50:33 +0200

    More fixes.

--- src/components/core/utils/tests/Export.spec.js
+++ src/components/core/utils/tests/Export.spec.js
@@ -1,38 +1,22 @@
 import DraalExport from '../Export.vue';
 
 describe('DraalExport', () => {
+    const text = 'Click Me!';
+
     function factory(propsData = {}) {
         return mountedComponentFactory(DraalExport, propsData, {
             slots: {
-                default: '<button type="button">Click Me!</button>'
+                default: `<button type="button">${text}</button>`
             }
         });
     }
 
-    const link = {
-        click: jest.fn(),
-    };
-
     it('file is available for download', async () => {
-        // jest.spyOn(document, 'createElement');
-        // jest.spyOn(document["body"], 'appendChild');
-        // .mockImplementation(() => link);
-
-        // GIVEN component
         const wrapper = factory({
             url: 'Foo',
             name: 'File.json'
         });
 
-        // console.log(wrapper.html());
-
-        const el = wrapper.find('button');
-        expect(el.text()).toEqual('Click Me!');
-        // jest.spyOn(input, 'click')
-        // input.trigger('click');
-
-        // await wrapper.vm.$nextTick();
-
-        // expect(link.click).toHaveBeenCalledTimes(1);
+        expect(wrapper.find('button').text()).toEqual(text);
     });
 });
